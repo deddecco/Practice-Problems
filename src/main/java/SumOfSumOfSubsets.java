12403776f1e5ad8679662d1039d78fac62dfec51
Andre da Silva <apdasil@emory.edu>
2024-11-08 19:28:45 -0500

    add comments to solution

--- src/main/java/SumOfSumOfSubsets.java
+++ src/main/java/SumOfSumOfSubsets.java
@@ -4,18 +4,23 @@ import java.util.Comparator;
 public class SumOfSumOfSubsets {
 
      public static int[][] generatePowerset(int[] set) {
+          // power set of empty set is empty
           if (set.length == 0) {
                return new int[][]{{}};
           }
 
+          // generate the powerset of [1, 2, 3,  ..., n-1]
           int[] subsetWithoutLast = new int[set.length - 1];
+          // copy that into the result set
           System.arraycopy(set, 0, subsetWithoutLast, 0, set.length - 1);
           int[][] subsetsWithoutLast = generatePowerset(subsetWithoutLast);
 
+          // the powerset of up to n is exactly twice the length of the powerset of up to n-1
           int[][] result = new int[subsetsWithoutLast.length * 2][];
 
           System.arraycopy(subsetsWithoutLast, 0, result, 0, subsetsWithoutLast.length);
 
+          // make a copy of each elemet of the powerset that was missing n, and put n there
           for (int i = 0; i < subsetsWithoutLast.length; i++) {
                int[] newSubset = new int[subsetsWithoutLast[i].length + 1];
                System.arraycopy(subsetsWithoutLast[i], 0, newSubset, 0, subsetsWithoutLast[i].length);
@@ -23,6 +28,8 @@ public class SumOfSumOfSubsets {
                result[subsetsWithoutLast.length + i] = newSubset;
           }
 
+          // {1, 2, 3} has {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}} = 8 sets in its power set
+          // the power set of {1, 2, 3} is 2 copies of the powerset of {1, 2}-- one with a 3 on the end, and one without a 3 on the end
           return result;
      }
 
